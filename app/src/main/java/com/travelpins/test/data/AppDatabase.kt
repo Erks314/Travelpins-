4ba7d5607d9f732b89167a95bf79d8a221e92bfb
Erks314 <mirko.rossi2@gmail.com>
2026-08-25 00:58:16 +0200

    Update AppDatabase.kt

--- app/src/main/java/com/travelpins/test/data/AppDatabase.kt
+++ app/src/main/java/com/travelpins/test/data/AppDatabase.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 
 @Database(
     entities = [Place::class, Category::class],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
@@ -25,7 +25,9 @@ abstract class AppDatabase : RoomDatabase() {
                     context.applicationContext,
                     AppDatabase::class.java,
                     "travelpins.db"
-                ).build().also { INSTANCE = it }
+                )
+                    .fallbackToDestructiveMigration(true)
+                    .build().also { INSTANCE = it }
             }
         }
     }
