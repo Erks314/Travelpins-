d7e6a4ba357e831b0c5dd129ea7f79f313be9214
Erks314 <mirko.rossi2@gmail.com>
2026-08-25 06:27:16 +0200

    Update AppDatabase.kt

--- app/src/main/java/com/travelpins/test/data/AppDatabase.kt
+++ app/src/main/java/com/travelpins/test/data/AppDatabase.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 
 @Database(
     entities = [Place::class, Category::class],
-    version = 2,
+    version = 3,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
