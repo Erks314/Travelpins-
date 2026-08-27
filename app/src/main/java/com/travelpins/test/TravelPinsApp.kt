b8076ca5e5684db8e7dae1dce51990b2cde9f023
Erks314 <mirko.rossi2@gmail.com>
2026-08-27 10:55:27 +0200

    Create TravelPinsApp.kt

---
+++ app/src/main/java/com/travelpins/test/TravelPinsApp.kt
@@ -0,0 +1,20 @@
+package com.travelpins.test
+
+import android.app.Application
+import com.travelpins.test.data.TravelPinsRepository
+import com.travelpins.test.importer.EnrichmentManager
+
+/**
+ * Avvia la coda di arricchimento all'avvio del processo,
+ * indipendentemente da quale schermata viene aperta.
+ */
+class TravelPinsApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        EnrichmentManager.start(
+            this,
+            TravelPinsRepository(this)
+        )
+    }
+}
