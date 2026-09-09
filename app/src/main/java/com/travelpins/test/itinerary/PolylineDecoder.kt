e702269618b81621b1eda38e921c54531bfcc368
Erks314 <mirko.rossi2@gmail.com>
2026-09-09 06:55:07 +0200

    Add PolylineDecoder for Google Maps polylines
    Implement a PolylineDecoder to decode Google Maps encoded polylines into a list of LatLng.

---
+++ app/src/main/java/com/travelpins/test/itinerary/PolylineDecoder.kt
@@ -0,0 +1,42 @@
+package com.travelpins.test.itinerary
+
+import com.google.android.gms.maps.model.LatLng
+
+/**
+ * Decodifica una polyline Google Maps encoded in una lista di LatLng.
+ * Algoritmo standard Google (https://developers.google.com/maps/documentation/utilities/polylinealgorithm)
+ */
+object PolylineDecoder {
+    fun decode(encoded: String): List<LatLng> {
+        val poly = mutableListOf<LatLng>()
+        var index = 0
+        var lat = 0
+        var lng = 0
+
+        while (index < encoded.length) {
+            var b: Int
+            var shift = 0
+            var result = 0
+            do {
+                b = encoded[index++].code - 63
+                result = result or ((b and 0x1f) shl shift)
+                shift += 5
+            } while (b >= 0x20)
+            val dlat = if ((result and 1) != 0) (result shr 1).inv() else result shr 1
+            lat += dlat
+
+            shift = 0
+            result = 0
+            do {
+                b = encoded[index++].code - 63
+                result = result or ((b and 0x1f) shl shift)
+                shift += 5
+            } while (b >= 0x20)
+            val dlng = if ((result and 1) != 0) (result shr 1).inv() else result shr 1
+            lng += dlng
+
+            poly.add(LatLng(lat / 1E5, lng / 1E5))
+        }
+        return poly
+    }
+}
